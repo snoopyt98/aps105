107f0e574d702b1b72d68f6e4716d46c724a6a09
Shizhang  Yin <yinshizh@p50.ecf.utoronto.ca>
2015-09-22 11:13:36 -0400

    first commit!

---
+++ lab0/Lab0.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Shizhang Yin
+ *
+ * Created on September 14, 2015, 12:57 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("This is my Lab 0 program!\n");
+    return (EXIT_SUCCESS);
+}
+
