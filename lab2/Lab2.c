abc77f4c05b4b12d4e8b01acf1201e6d263c0103
Shizhang  Yin <yinshizh@p50.ecf.utoronto.ca>
2015-09-22 11:43:07 -0400

    add lab2

---
+++ lab2/Lab2.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   Lab2.c
+ * Author: yinshizh
+ *
+ * Created on September 22, 2015, 11:40 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
