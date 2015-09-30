190d4b43a14d98b44c97ae3be03e5571095fd1b2
Shizhang  Yin <yinshizh@p29.ecf.utoronto.ca>
2015-09-30 11:41:20 -0400

    create lab3

---
+++ lab3/Lab3part1.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   Lab3part1.c
+ * Author: yinshizh
+ *
+ * Created on September 30, 2015, 11:40 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
