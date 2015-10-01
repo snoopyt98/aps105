4a78309d67e178dcadf430dc56ef2e9bd0d427b1
Shizhang  Yin <yinshizh@p102.ecf.utoronto.ca>
2015-10-01 14:00:33 -0400

    fix# Please enter the commit message for your changes. Lines starting

--- lab3/Lab3Part2.c
+++ lab3/Lab3Part2.c
@@ -9,27 +9,23 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(void) 
-{
+int main(void) {
     int randomSeed, numberToguess, inputNumber;
-    bool processState=true;
+    bool processState = true;
     printf("Enter seed: ");
-    scanf("%d",&randomSeed);
+    scanf("%d", &randomSeed);
     srand(randomSeed);
-    while(processState==true)
-    {
-        numberToguess=(rand()%7)+1;
+    while (processState == true) {
+        numberToguess = (rand() % 7) + 1;
         printf("Guess a number from 1 to 7 (<=0 to quit): ");
-        scanf("%d",&inputNumber);
-        printf("Number picked: %d\n",numberToguess);
-        if(inputNumber<=0)
-        {
+        scanf("%d", &inputNumber);
+        printf("Number picked: %d\n", numberToguess);
+        if (inputNumber <= 0) {
             printf("Goodbye");
-            processState=false;
-        }
-        else if(inputNumber>7)
+            processState = false;
+        } else if (inputNumber > 7)
             printf("Number too high, guess again!\n");
-        else if(inputNumber==numberToguess)
+        else if (inputNumber == numberToguess)
             printf("Good guess!\n");
         else
             printf("Sorry, try again!\n");
