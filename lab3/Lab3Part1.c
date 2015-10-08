73b0d38a3af4bb292e68c5db60cec347c3e11f90
Shizhang  Yin <yinshizh@p50.ecf.utoronto.ca>
2015-10-08 11:11:49 -0400

    commit on lab3

--- lab3/Lab3Part1.c
+++ lab3/Lab3Part1.c
@@ -14,12 +14,12 @@ int main(void)
     double firstNum, secondNum;
     char inputChar; // Declare variables
     
-    printf("Enter first number: ");
+    printf("Enter first number: "); // Ask for input numbers
     scanf("%lf", &firstNum);
     printf("Enter second number: ");
     scanf("%lf", &secondNum);
-    printf("Enter calculation command (one of a, s, m, or d): ");
-    scanf(" %c", &inputChar); // Ask for inputs
+    printf("Enter calculation command (one of a, s, m, or d): "); // Ask for inputmode
+    scanf(" %c", &inputChar); 
     
     if (inputChar == 'a') // Addition case
         printf("Sum of %.2f and %.2f is %.2f", firstNum, secondNum, firstNum + secondNum);
@@ -29,11 +29,11 @@ int main(void)
         printf("Product of %.2f and %.2f is %.2f", firstNum, secondNum, firstNum * secondNum);
     else if (inputChar == 'd') // Division case
     {
-        if (secondNum != 0) // 
+        if (secondNum != 0) // The case  where denominator not equals to zero  
             printf("Division of %.2f by %.2f is %.2f", firstNum, secondNum, firstNum / secondNum);
-        else
+        else // The case where denominator equals to zero
             printf("Error, trying to divide by zero");
-    } else
+    } else // The case where the input is unknown
         printf("Error, unknown calculation command given");
     return (EXIT_SUCCESS);
 }
--- lab3/Lab3Part2.c
+++ lab3/Lab3Part2.c
@@ -9,40 +9,46 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(void) {
+int main(void)
+ {
+    
     int randomSeed, numberToguess, inputNumber;
     bool processState = true;
-    bool reRandom=true;
+    bool reRandom=true; // Declare variables
+    
     printf("Enter seed: ");
-    scanf("%d", &randomSeed);
-    srand(randomSeed);
-    while (processState == true) {
-        if(reRandom==true)
+    scanf("%d", &randomSeed); // Get the seed for rand() from user
+    srand(randomSeed); // Set the seed with srand()
+    while (processState == true) // Determain if the program should exit
+    {
+        if(reRandom==true) // Determain if the program needs a new random number
         {
-        numberToguess = (rand() % 7) + 1;
+        numberToguess = (rand() % 7) + 1; // Get a random number from 1 to 7
         }
-        reRandom=true;
+        
+	reRandom=true; // Ask for a new random number next loop
         printf("Guess a number from 1 to 7 (<=0 to quit): ");
-        scanf("%d", &inputNumber);
-        if (inputNumber <= 0) 
+        scanf("%d", &inputNumber); // Ask for user input
+       
+	 if (inputNumber <= 0)
         {
             printf("Goodbye");
-            processState = false;
+            processState = false; // Exit the loop
         } 
         else if (inputNumber > 7)
         {
             printf("Number too high, guess again!\n");
-            reRandom=false;
+            reRandom=false; // Stop regenarate a new random number
         }
         else if (inputNumber == numberToguess) 
         {
             printf("Number picked: %d\n", numberToguess);
-            printf("Good guess!\n");
+            printf("Good guess!\n"); // The user guessed the right number
         } 
         else 
         {
             printf("Number picked: %d\n", numberToguess);
-            printf("Sorry, try again!\n");
+            printf("Sorry, try again!\n"); // The user guessed the wrong number
         }
     }
     return (EXIT_SUCCESS);
