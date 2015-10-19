3284ae7636ef9e808fb3aca7ae5b4ef3be5ee6ba
Shizhang  Yin <yinshizh@remote.ecf.utoronto.ca>
2015-10-19 12:38:38 -0400

    save work

--- lab4/Lab4.c
+++ lab4/Lab4.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
-static const int CONST_E=2.71828182845904524;
-double gaussFunc(double x, double spread);
+static const int CONST_E=2.71828182845904524;//declare constant e
+
+double gaussFunc(double x, double spread);//declare function gaussFunc()
 
 int main(void)
 {
-    double spreadNum, limitA, limitB, midX, outResult=0;
+    double spreadNum, limitA, limitB, midX, outResult=0;//declare variable
     int inputN, i;
     do
     {
-        printf("Enter the spread value (must be >0): ");
+        printf("Enter the spread value (must be >0): ");//ask for spread value
         scanf("%lf",&spreadNum);
     }
     while(spreadNum<=0);
@@ -22,30 +23,32 @@ int main(void)
         if(inputN<1)
         {
             printf("exiting.\n");
-            return 0;
+            return 0;//exit
         }
         do
         {
-            printf("Enter the interval of integration (a b): ");
+            printf("Enter the interval of integration (a b): ");//ask for the range of a closed integral
             scanf("%lf %lf", &limitA, &limitB);
             if(limitA>limitB)
                 printf("Invalid interval!\n");
         }
         while(limitA>limitB);
-        midX=limitA+((limitB-limitA)/(2*inputN));
+        midX=limitA+((limitB-limitA)/(2*inputN));//find the first midX
         for(i=0;i<inputN;i++)
         {
-            midX=((limitB-limitA)/(inputN))+midX;
-            outResult=outResult+gaussFunc(midX,spreadNum)*((limitB-limitA)/inputN);
+            if(i!=0)
+                midX=((limitB-limitA)/(inputN))+midX;//find each midX
+            printf("%f,%f",midX,gaussFunc(midX,spreadNum));
+            outResult=outResult+gaussFunc(midX,spreadNum)*((limitB-limitA)/inputN);//calculate area and add together
         }
-        printf("Integral of Gaussian with spread %.3f on [%.3f, %.3f] with n = %d is: %.3f\n", spreadNum, limitA, limitB, inputN, outResult);
+        printf("Integral of Gaussian with spread %.3f on [%.3f, %.3f] with n = %d is: %.3f\n", spreadNum, limitA, limitB, inputN, outResult);//print result
         outResult=0;
     }
 }
 
-double gaussFunc(double x, double spread)
+double gaussFunc(double x, double spread)//Find y value of given x
 {
-    double outArea;
-    outArea=pow(CONST_E, -(x*x)/spread);
-    return outArea;
+    double outY;
+    outY=exp(-(x*x)/spread);
+    return outY;
 }
